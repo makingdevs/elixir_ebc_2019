0f33c8b735dd33a7f3ad3a3c4f2fc4bdbe238743
neodevelop <juan@makingdevs.com>
2018-12-22 10:43:57 -0600

    functions: headers, patter matching, default values

--- dia2/greeter.ex
+++ dia2/greeter.ex
@@ -1,23 +1,25 @@
 defmodule Greeter do
 
-  def hello(names) when is_list(names) do
+  def hello(names, language \\ "es")
+  def hello(names, language) when is_list(names) do
     names
     |> Enum.join(", ")
-    |> hello
+    |> hello(language)
   end
 
-  def hello(name) when is_binary(name) do
-    phrase() <> " " <> name
+  def hello(name, language) when is_binary(name) do
+    phrase(language) <> " " <> name
   end
 
-  def hello(%{lastname: lastname, name: name} = _person) do
-    phrase() <> " #{name} #{lastname}"
+  def hello(%{lastname: lastname, name: name} = _person, language) do
+    phrase(language) <> " #{name} #{lastname}"
   end
 
-  def hello(%{name: name} = _person) do
-    phrase() <> " #{name}"
+  def hello(%{name: name} = _person, language) do
+    phrase(language) <> " #{name}"
   end
 
-  defp phrase(), do: "Hello"
+  defp phrase("es"), do: "Hola"
+  defp phrase("en"), do: "Hello"
 
 end
