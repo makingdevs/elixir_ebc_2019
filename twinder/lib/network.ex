5458784e347349b0e089e3a7ec678d69ac993a6a
neodevelop <juan@makingdevs.com>
2018-12-22 12:13:26 -0600

    Make a pipe of tasks to obtain users...

--- twinder/lib/network.ex
+++ twinder/lib/network.ex
@@ -6,6 +6,23 @@ defmodule Twinder.Network do
     @url
     |> String.replace("{username}", username)
     |> HTTPoison.get!()
+    |> parse_response
+    |> obtain_usernames
+    |> generate_users
+  end
+
+  defp parse_response(%HTTPoison.Response{body: body}) do
+    body |> Poison.decode!
+  end
+
+  defp obtain_usernames(list_of_users) do
+    list_of_users
+    |> Enum.map(fn m -> m["login"] end)
+  end
+
+  defp generate_users(list_of_usernames) do
+    list_of_usernames
+    |> Enum.map(&Twinder.User.new/1)
   end
 
 end
