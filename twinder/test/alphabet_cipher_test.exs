a39a2f12cd80feb36731773c1a0e128d2b6867c9
neodevelop <juan@makingdevs.com>
2019-01-07 10:27:55 -0600

    Agrego dops pruebas

--- twinder/test/alphabet_cipher_test.exs
+++ twinder/test/alphabet_cipher_test.exs
@@ -1,10 +1,17 @@
 defmodule Twinder.AlphabetCipher.Test do
   use ExUnit.Case
 
-  test "Cipher a message" do
+  test "encode a message" do
     message = "meetmebythetree"
     secret = "scone"
     result = AlphabetCipher.encode(message, secret)
     assert result == "egsgqwtahuiljgs"
+  end
+
+  test "decode a message" do
+    secret_message = "egsgqwtahuiljgs"
+    secret = "scone"
+    result = AlphabetCipher.decode(secret_message, secret)
+    assert result == "meetmebythetree"
   end
 end
