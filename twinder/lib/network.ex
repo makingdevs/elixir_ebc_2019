2fba2a8289470683c9b693b2edeb9bb8cf3aac5b
neodevelop <juan@makingdevs.com>
2019-01-07 09:31:51 -0600

    Refactor: uso de `import` y `alias`

--- twinder/lib/network.ex
+++ twinder/lib/network.ex
@@ -1,28 +1,32 @@
 defmodule Twinder.Network do
+  alias Twinder.User
+  alias HTTPoison.Response
+  import String, only: [replace: 3]
+  import Enum, only: [map: 2]
 
   @url "https://api.github.com/users/{username}/followers"
 
   def get_users_of(username) do
     @url
-    |> String.replace("{username}", username)
+    |> replace("{username}", username)
     |> HTTPoison.get!()
     |> parse_response
     |> obtain_usernames
     |> generate_users
   end
 
-  defp parse_response(%HTTPoison.Response{body: body}) do
+  defp parse_response(%Response{body: body}) do
     body |> Poison.decode!
   end
 
   defp obtain_usernames(list_of_users) do
     list_of_users
-    |> Enum.map(fn m -> m["login"] end)
+    |> map(fn m -> m["login"] end)
   end
 
   defp generate_users(list_of_usernames) do
     list_of_usernames
-    |> Enum.map(&Twinder.User.new/1)
+    |> map(&User.new/1)
   end
 
 end
