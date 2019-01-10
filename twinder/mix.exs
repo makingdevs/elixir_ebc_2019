5b7567a92ba29b7b680de096cacebc2b24823ff3
neodevelop <juan@makingdevs.com>
2019-01-10 09:51:32 -0600

    Agrego plug y cowboy

--- twinder/mix.exs
+++ twinder/mix.exs
@@ -23,6 +23,8 @@ defmodule Twinder.MixProject do
     [
       {:httpoison, "~> 1.4"},
       {:poison, "~> 3.1"},
+      {:cowboy, "~> 2.6"},
+      {:plug_cowboy, "~> 2.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
