4bffb310212e3098545bd7d364368f3ae422fd6d
neodevelop <juan@makingdevs.com>
2019-01-11 12:03:02 -0600

    Agrego la relación en BD de user con posts

---
+++ ebc_webapp/priv/repo/migrations/20190111175708_add_posts_to_user.exs
@@ -0,0 +1,10 @@
+defmodule EbcWebapp.Repo.Migrations.AddPostsToUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:posts) do
+      add :user_id, references(:users)
+    end
+    create index(:posts, [:user_id])
+  end
+end
