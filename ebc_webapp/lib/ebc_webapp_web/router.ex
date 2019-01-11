176981f84c169b5875c1e15921d72ca992fa390d
neodevelop <juan@makingdevs.com>
2019-01-11 12:02:46 -0600

    Agrego resources de post al router

--- ebc_webapp/lib/ebc_webapp_web/router.ex
+++ ebc_webapp/lib/ebc_webapp_web/router.ex
@@ -19,6 +19,7 @@ defmodule EbcWebappWeb.Router do
     get "/", PageController, :index
     get "/hello", PageController, :hello
     resources "/users", UserController
+    resources "/posts", PostController
   end
 
   # Other scopes may use custom stacks.
