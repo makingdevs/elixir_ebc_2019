f35a91d452c829bb9a9064c15dbec1c5907fbeb9
neodevelop <juan@makingdevs.com>
2019-01-11 11:52:28 -0600

    Agrego nuevas funciones en el view

--- ebc_webapp/lib/ebc_webapp_web/views/user_view.ex
+++ ebc_webapp/lib/ebc_webapp_web/views/user_view.ex
@@ -1,3 +1,15 @@
 defmodule EbcWebappWeb.UserView do
   use EbcWebappWeb, :view
+
+  def current_date() do
+    today =  Date.utc_today
+    "#{today.day} - #{today.month} - #{today.year}"
+  end
+
+  def count_users(users) do
+    """
+    There are #{Enum.count(users)} users.
+    """
+  end
+
 end
